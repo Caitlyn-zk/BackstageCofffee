d23771e5ee08feeb4e0d3e386f77901e8c12d848
Caitlyn-zk <2655816316@qq.com>
2019-12-05 10:12:43 +0800

    后台项目环境搭建完成，第一次上传 邹凯

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+			<el-button class="padding-10 font-36 line-36" type="">按钮</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
